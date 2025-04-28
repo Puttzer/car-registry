79654ae98222db7356a3edd1948799a879fe18f4
Patrick Carragher <carragher@live.se>
2025-04-28 16:17:30 +0200

    Get All brands from brands table.

--- src/main/java/com/example/registry/dao/BrandDAO.java
+++ src/main/java/com/example/registry/dao/BrandDAO.java
@@ -1,6 +1,29 @@
 package com.example.registry.dao;
 
+import com.example.registry.model.Brand;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrandDAO {
 
-
+    public List<Brand> getAllBrands() throws SQLException {
+        List<Brand> brands = new ArrayList<>();
+        String sql = "SELECT id, name FROM brands";
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement state = conn.prepareStatement(sql);
+            ResultSet res = state.executeQuery()){
+            while(res.next()) {
+                int id = res.getInt("id");
+                String name = res.getString("name");
+                Brand brand = new Brand(id, name);
+                brands.add(brand);
+            }
+        }
+        return brands;
+    }
 }
